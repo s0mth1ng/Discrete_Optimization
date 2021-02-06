d9873f89569f9648626469f7a3bf9ef906e400a3
maxim <Maxim Ivanov>
2021-02-06 13:30:56 +0300

    added dp solution (N * K); first two task completed

--- week2/knapsack/solver.py
+++ week2/knapsack/solver.py
@@ -28,6 +28,40 @@ def brute_force_solution(items, capacity):
     return output_data
 
 
+def dp_solution(items, capacity):
+    item_count = len(items)
+    dp = [[(0, 0) for _ in range(capacity + 1)] for i in range(item_count)]
+    
+    # start values
+    for i in range(items[0].weight, capacity + 1):
+        dp[0][i] = (items[0].value, 1)
+    
+    # reccurence
+    for i in range(1, item_count):
+        for max_weight in range(capacity + 1):
+            if items[i].weight > max_weight:
+                dp[i][max_weight] = (dp[i - 1][max_weight][0], 0)
+            elif dp[i - 1][max_weight][0] < dp[i - 1][max_weight - items[i].weight][0] + items[i].value:
+                dp[i][max_weight] = (dp[i - 1][max_weight - items[i].weight][0] + items[i].value, 1)
+            else:
+                dp[i][max_weight] = (dp[i - 1][max_weight][0], 0)
+    
+    taken = [0] * item_count
+    cur_item = item_count - 1
+    cur_weight = capacity
+    while cur_item >= 0:
+        if dp[cur_item][cur_weight][1]:
+            taken[cur_item] = 1
+            cur_weight -= items[cur_item].weight
+        cur_item -= 1
+    
+    output_data = str(dp[item_count - 1][capacity][0]) + ' 1\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
+    
+
+
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -47,6 +81,9 @@ def solve_it(input_data):
 
     if item_count <= 22:
         return brute_force_solution(items, capacity)
+
+    if item_count * capacity <= 2 * 10 ** 7:
+        return dp_solution(items, capacity)
 
     # a trivial algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
